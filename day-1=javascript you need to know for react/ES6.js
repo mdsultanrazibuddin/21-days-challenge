6f831b7b5eea5c7af7c019ca975dc0e062008ed0
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-10-04 15:51:58 +0600

    added

---
+++ day-1=javascript you need to know for react/ES6.js
@@ -0,0 +1,29 @@
+const arr =[20, 23, 24, 11, 33,44, 78]
+
+const student ={
+    name: 'kasem',
+    age: 20,
+    movies: ['sakib khan, superstar, ajaira']
+}
+//  template string 
+const about = `My name is ${student.name} age of ${student.age} has number ${arr[0]} also like movies ${student.movies[0]}`
+
+// arrow function
+
+const get = () => 55
+const add = num => num + 10
+
+const doMath = (num1, num2) => {
+    const sum = num1 + num2 
+    return sum
+}
+
+const output = doMath(52, 25)
+console.log(output);
+
+
+// spread operator
+
+const newNumber = [...arr, 77]
+console.log(newNumber);
+
